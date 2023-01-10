0a2384b4fbf222d35e08e2fc6c4fc4916646b545
Raul <jimenezraul1981@gmail.com>
2023-01-10 13:51:52 -0500

    created indexedDB hook function

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# IndexDB Promise
+# IndexedDB Promise
 
 ## What is it?
 
-This is a IndexDB hook for React. IndexDB is a browser API that allows you to store data in the browser. It is a great way to store data that you want to persist between sessions. It is also a great way to store data that you want to use offline.
+This is a IndexedDB hook for React. IndexedDB is a browser API that allows you to store data in the browser. It is a great way to store data that you want to persist between sessions. It is also a great way to store data that you want to use offline.
 
 ## How do I use it?
 
---
+++ useIdbpromise.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { idbPromise } from "../utils/helpers";
+
+const useIdbPromise = (table, method, params) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await idbPromise(table, method, params);
+        let item;
+        if (response.length > 0) {
+          item = response[0];
+        } 
+        setData(item);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [table, method, params]);
+
+  return { data, loading, error };
+};
+
+export default useIdbPromise;
